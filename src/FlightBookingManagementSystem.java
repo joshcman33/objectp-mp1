150ef6f1c1d7bb2ad916ddf0fa0a2f018bbd569d
Sherlock Baldeo <js_baldeo@yahoo.com>
2012-07-07 14:23:55 +0800

    MainMenu display

--- src/FlightBookingManagementSystem.java
+++ src/FlightBookingManagementSystem.java
@@ -7,8 +7,30 @@
  *
  * @author Joshua C. Manansala
  */
+import java.util.Scanner;
+
 public class FlightBookingManagementSystem {
     private Flight[] flightList;
     private int flightCount;
     private Passenger defaultPassenger;
+    private int input;
+
+
+
+    System.out.ln("FLIGHT BOOKING SYSTEM");
+    System.out.ln("[1]Admin Module");
+    System.out.ln("[2]Booking Module");
+    System.out.ln("[3]Exit");
+
+    Scanner scan = new Scanner(System.in);
+    input = scan.nextLine();
+
+
+
+
+
+
+
+
+
 }
