d0215d2cdc36c98e1e4d8cfce305a47b3fbb914f
Joshua C. Manansala <manansala.joshua@gmail.com>
2012-07-07 21:56:46 +0800

    Preliminary Methods for some classes

--- src/FlightBookingManagementSystem.java
+++ src/FlightBookingManagementSystem.java
@@ -5,18 +5,35 @@
 
 /**
  *
- * @author  John Sherlock Baldeo
- *          Joshua C. Manansala
- * 
+ * @author John Sherlock Baldeo Joshua C. Manansala
+ *
  */
 public class FlightBookingManagementSystem {
+
     private Flight[] flightList;
     private int flightCount;
-    private Passenger defaultPassenger;
-    
-    public FlightBookingManagementSystem(Flight[] flightList, int flightCount, Passenger defaultPassenger) {
+    private Passenger currentUser;
+    private Passenger defaultUser;
+
+    public FlightBookingManagementSystem(Flight[] flightList, int flightCount) {
         this.flightList = flightList;
         this.flightCount = flightCount;
-        this.defaultPassenger = defaultPassenger;
+        currentUser = defaultUser = new Passenger();
+    }
+
+    public int getSize() {
+        return flightCount;
+    }
+
+    public void login(Administrator admin, String password) {
+        if (admin.login(password)) {
+            currentUser = admin;
+            System.out.println("Administrator mode logged in.");
+        }
+    }
+    
+    public void logout() {
+        currentUser = defaultUser;
+        System.out.println("Administrator mode logged out. Going back to default user mode.");
     }
 }
