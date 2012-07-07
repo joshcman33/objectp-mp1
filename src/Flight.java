edc49d3464bd01c9f8f76d6f274fb5f7c7eb3935
Joshua C. Manansala <Joshua C. Manansala@JCMLPC-W7>
2012-07-07 11:35:05 +0800

    Preliminary Commit for OBJECTP MP 1

---
+++ src/Flight.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Joshua C. Manansala
+ */
+public class Flight {
+    private String flightNumber;
+    private String flightAirline;
+    private String flightOrigin;
+    private String flightDestination;
+    private int flightDepartureDate;
+    private int flightDepartureTime;
+    private int flightMaximumCapacityOfPassengers;
+    private User[] passengerList;    
+}
